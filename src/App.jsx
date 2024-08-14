d9e00407954a1a704f402020aa90e12f054a982a
myjungwu <baek.myungsuk@gmail.com>
2024-08-14 10:54:20 +0900

    EmployeeComponent add

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import HeaderComponent from "src/components/common/HeaderComponent";
 import FooterComponent from "src/components/common/FooterComponent";
 import ListEmployeeComponent from "src/components/ListEmployeeComponent";
+import EmployeeComponent from "src/components/EmployeeComponent";
 
 function App() {
   return (
@@ -15,6 +16,10 @@ function App() {
           <Route path="/" element={<ListEmployeeComponent />}></Route>
           {/* // http://localhost:3000/employees */}
           <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+          {/* // http://localhost:3000/add-employee */}
+          <Route path="/add-employee" element={<EmployeeComponent />}></Route>
+          {/* // http://localhost:3000/edit-employee/1 */}
+          <Route path="/edit-employee/:id" element={<EmployeeComponent />}></Route>
         </Routes>
         <FooterComponent />
       </BrowserRouter>
