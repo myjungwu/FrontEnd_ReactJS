92c46c98c66ec91e68d581b2681a1a5ab3c2a14c
myjungwu <baek.myungsuk@gmail.com>
2024-08-14 10:12:11 +0900

    Header컴포넌트 추가

---
+++ src/components/common/HeaderComponent.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+                <a className="navbar-brand" href="https://react.dev/">Employee Management System</a>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <NavLink className='nav-link' to='/employees'>Employees</NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink className='nav-link' to='/departments'>Departments</NavLink>
+                    </li>
+                  </ul>
+              </div>
+              <div className="navbar-brand">Dev Mode</div>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
