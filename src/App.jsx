1e4962bfaeff738fd8b93311fd80b51aa589a860
myjungwu <baek.myungsuk@gmail.com>
2024-08-14 10:41:31 +0900

    ListEmployeeComponent add

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HeaderComponent from "src/components/common/HeaderComponent";
 import FooterComponent from "src/components/common/FooterComponent";
+import ListEmployeeComponent from "src/components/ListEmployeeComponent";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <HeaderComponent />
+        <Routes>
+          {/* // http://localhost:3000 */}
+          <Route path="/" element={<ListEmployeeComponent />}></Route>
+          {/* // http://localhost:3000/employees */}
+          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+        </Routes>
         <FooterComponent />
       </BrowserRouter>
     </>
