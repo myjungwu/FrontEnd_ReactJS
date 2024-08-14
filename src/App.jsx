79673e26605e089550e97d38d5907f5ab607c478
myjungwu <baek.myungsuk@gmail.com>
2024-08-14 11:21:40 +0900

    DepartmentComponent add

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import FooterComponent from "src/components/common/FooterComponent";
 import ListEmployeeComponent from "src/components/ListEmployeeComponent";
 import EmployeeComponent from "src/components/EmployeeComponent";
 import ListDepartmentComponent from "src/components/ListDepartmentComponent";
+import DepartmentComponent from "src/components/DepartmentComponent";
 
 function App() {
   return (
@@ -22,8 +23,11 @@ function App() {
           {/* // http://localhost:3000/edit-employee/1 */}
           <Route path="/edit-employee/:id" element={<EmployeeComponent />}></Route>
           {/* // http://localhost:3000/departments */}
-          <Route path='/departments' element = { <ListDepartmentComponent />}></Route>
-
+          <Route path='/departments' element = { <ListDepartmentComponent />}></Route>          
+          {/* // http://localhost:3000/add-department */}
+          <Route path='/add-department' element = { <DepartmentComponent /> }></Route>
+          {/* // http://localhost:3000/edit-department/1 */}
+          <Route path='/edit-department/:id' element = { <DepartmentComponent />}></Route>          
         </Routes>
         <FooterComponent />
       </BrowserRouter>
