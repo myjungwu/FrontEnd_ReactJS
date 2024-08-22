41edae0fcd81829380630e58e77e211f32ace26b
myjungwu <baek.myungsuk@gmail.com>
2024-08-22 14:27:09 +0900

    api0.3

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const REST_API_BASE_URL =  import.meta.env.VITE_APIURL
+console.log(`REST_API_BASE_URL = ${REST_API_BASE_URL}`)
 //'http://localhost:8080/api'
 
 const REST_API_URL = `${REST_API_BASE_URL}/employees`
+console.log(`REST_API_URL = ${REST_API_URL}`)
 //'http://localhost:8080/api/employees';
 
 //export const listEmployees = () => axios.get(REST_API_BASE_URL);
