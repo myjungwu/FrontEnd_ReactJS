52ffa72db631f646da3b34f4f3803dc8e6a35f46
myjungwu <baek.myungsuk@gmail.com>
2024-08-23 13:31:12 +0900

    env0.3

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  server: {
-    port: 3000
-  },
-  //absolute path 
-  resolve: {
-    alias: {
-      src: "/src",
-    }
-  },   
-})
+  return {
+    plugins: [react()],
+
+    server: {
+      port: 3000,
+    },
+    //absolute path
+    resolve: {
+      alias: {
+        src: "/src",
+      },
+    },
+    define: {
+      VITE_APIURL : JSON.stringify(env.VITE_APIURL),
+    },
+  };
+});
