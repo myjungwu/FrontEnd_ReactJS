c26e6d664ac01fb37eb5b08893d7c7b7ed4b603a
myjungwu <baek.myungsuk@gmail.com>
2024-08-26 13:49:38 +0900

    t0.4

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 // export default defineConfig(() => {
 export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
@@ -18,9 +18,9 @@ export default defineConfig(({ mode }) => {
         src: "/src",
       },
     },
-    // define: {
-    //   VITE_APIURL: JSON.stringify(env.VITE_APIURL),
-    // },
+    define: {
+      VITE_APIURL: JSON.stringify(env.VITE_APIURL),
+    },
   };
   
 });
